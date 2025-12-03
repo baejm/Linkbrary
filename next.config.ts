c79bfe58d087e8d7aa9e8fc432cf1cf5e082c454
DESKTOP-KDUIRM0\jukma <jukman81@gmail.com>
2025-12-03 22:21:39 +0900

    코드래빗 적용

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,11 @@ const nextConfig = {
         hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "assets.nflxext.com",
+        pathname: "/**",
+      },
     ],
   },
 };
